87c24db8b39a7312023d545cc3a32448ce3ae93c
smcc977 <sammcc977@gmail.com>
2025-11-11 15:16:02 -0800

    Adding init of node

--- Node.py
+++ Node.py
@@ -1,3 +1,7 @@
 class Node:
-    def __init__(self,name):
-        self.name=name
+    def __init__(self, id, graph ):
+        self.id=id
+        self.neighbours=self.findNeighbours(graph)
+
+    def findNeighbours(self, graph):
+        return graph.neighbors(self.id)
